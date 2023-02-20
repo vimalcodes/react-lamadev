f36d8f57c45d27cbc531ceea0701d427d3fb1daa
Vimal <92043391+vimalcodes@users.noreply.github.com>
2023-02-20 11:53:46 +0800

    First Commit

---
+++ src/components/topbar/Topbar.jsx
@@ -0,0 +1,29 @@
+import "./topbar.scss"
+import {Person,Mail} from "@mui/icons-material"
+
+export default function Topbar({ menuOpen, setMenuOpen}) {
+  return (
+    <div className={"topbar " + (menuOpen && "active")}>
+      <div className="wrapper">
+        <div className="left">
+          <a href="#intro" className="logo">.genius</a>
+          <div className="itemContainer">
+            <Person className="icon"/>
+            <span>+0005566</span>
+          </div>
+          <div className="itemContainer">
+            <Mail className="icon"/>
+            <span>banana@oranges</span>
+          </div>
+        </div>
+        <div className="right">
+          <div className="hamburger" onClick={()=>setMenuOpen(!menuOpen)}>
+            <span className="line1"></span>
+            <span className="line2"></span>
+            <span className="line3"></span>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
